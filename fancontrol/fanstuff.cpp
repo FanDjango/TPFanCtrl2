aca7d516947a6e822acc769aede953320aa2af2c
FanDjango <51046875+FanDjango@users.noreply.github.com>
2025-10-21 11:57:42 +0200

    Improve Ramp Up/Down logic

--- fancontrol/fanstuff.cpp
+++ fancontrol/fanstuff.cpp
@@ -322,18 +322,17 @@ bool FANCONTROL::HandleData(void) {
 void FANCONTROL::SmartControl(void) {
 	char obuf[256] = "";
 	int i, newfanctrl = -1, levelIndex = -1, fanctrl = this->State.FanCtrl;
+	int hup, hdown;
 
 	if (this->PreviousMode == 1) {
 		sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "Change Mode from BIOS->");
 		sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "Smart, recalculate fan speed");
-
 		this->Trace(obuf);
 	}
 
 	if (this->PreviousMode == 3) {
 		sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "Change Mode from Manual->");
 		sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "Smart, recalculate fan speed");
-
 		this->Trace(obuf);
 	}
 
@@ -346,17 +345,25 @@ void FANCONTROL::SmartControl(void) {
 	//5 Level = 95   64  0   0 
 	//6 Level = 105 128  0   0 
 
-	newfanctrl = -1;
-
 	if ((fanctrl > 7 && (fanctrl != 64 || !Lev64Norm)) || this->PreviousMode == 3 || this->PreviousMode == 1) {
-		newfanctrl = 0;
-		levelIndex = 0;
 		fanctrl = 0;
+		levelIndex = 0;
+		newfanctrl = 0;
+	}
+
+	// ignore hysteresis on first time setting fan
+	if (this->LastSmartLevel < 0) {
+		hup = 0;
+		hdown = 0;
+	}
+	else {
+		hup = this->SmartLevels[this->LastSmartLevel].hystUp;
+		hdown = this->SmartLevels[this->LastSmartLevel].hystDown;
 	}
 
 	// Check for fan speed ramp upwards
 	for (i = 0; this->SmartLevels[i].temp != -1; i++) {
-		if (this->MaxTemp >= this->SmartLevels[i].temp && this->SmartLevels[i].fan >= fanctrl) {
+		if (this->MaxTemp >= this->SmartLevels[i].temp + hup && this->SmartLevels[i].fan >= fanctrl) {
 			newfanctrl = this->SmartLevels[i].fan;
 			levelIndex = i;
 		}
@@ -365,7 +372,7 @@ void FANCONTROL::SmartControl(void) {
 	// Check for fan speed ramp downwards
 	if (newfanctrl == -1) {
 		for (i = 0; this->SmartLevels[i].temp != -1; i++) {
-			if (this->MaxTemp <= this->SmartLevels[i].temp && this->SmartLevels[i].fan < fanctrl) {
+			if (this->MaxTemp <= this->SmartLevels[i].temp - hdown && this->SmartLevels[i].fan < fanctrl) {
 				newfanctrl = this->SmartLevels[i].fan;
 				levelIndex = i;
 				break;
@@ -373,31 +380,7 @@ void FANCONTROL::SmartControl(void) {
 		}
 	}
 
-	// fan speed ramp up or down?
 	if (newfanctrl != -1 && newfanctrl != this->State.FanCtrl) {
-		//if (newfanctrl == 0x80) {  
-			// switch to BIOS-auto mode
-		//	this->ModeToDialog(1);    
-		//}
-
-		// do not change if hyst zone, determine which hyst zone if we are in based on previous temp
-		// DO NOT HAVE HYSTERESIS OVERLAP WITH FAN TEMPS IN CONFIG!
-		SMARTENTRY newLevel = this->SmartLevels[levelIndex];
-		if (this->LastSmartLevel < 0) { // ignore hyst on first time setting fan
-			this->LastSmartLevel = levelIndex;
-			this->SetFan("Smart", newfanctrl);
-			return;
-		}
-
-		if (this->MaxTemp < this->SmartLevels[this->LastSmartLevel].temp) {
-			if (this->MaxTemp > newLevel.temp - newLevel.hystDown)
-				return; // cooling
-		}
-		else {
-			if (this->MaxTemp < newLevel.temp + newLevel.hystUp)
-				return; // rising 
-		}
-
 		this->LastSmartLevel = levelIndex;
 		this->SetFan("Smart", newfanctrl);
 	}
