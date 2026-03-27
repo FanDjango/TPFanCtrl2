438249479f51b702357a10cf706b0ba7844dba99
FanDjango <51046875+FanDjango@users.noreply.github.com>
2026-03-27 21:12:21 +0100

    v

--- fancontrol/fancontrol.h
+++ fancontrol/fancontrol.h
@@ -25,8 +25,8 @@
 #include "winstuff.h"
 #include "TaskbarTextIcon.h"
 
-constexpr const char* FANCONTROLVERSIONS = "2.3.12 Single Fan";
-constexpr const char* FANCONTROLVERSIOND = "2.3.12 Dual Fan";
+constexpr const char* FANCONTROLVERSIONS = "2.3.13 Single Fan";
+constexpr const char* FANCONTROLVERSIOND = "2.3.13 Dual Fan";
 
 //Pipe name format - \\.\pipe\pipename
 #define g_szPipeName "\\\\.\\Pipe\\TPFanControl01"  //Name given to the pipe
